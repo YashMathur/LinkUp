7d743ec916c72f727b71aaac374b19698aea184b
Archer Zhang <archer.xn@live.cn>
2017-09-16 15:51:25 -0400

    Fixed main activity

--- android/app/src/main/java/linkup/geese/io/linkup/MainActivity.java
+++ android/app/src/main/java/linkup/geese/io/linkup/MainActivity.java
@@ -10,7 +10,10 @@ import java.util.Map;
 import linkup.geese.io.linkup.cache.Cache;
 import linkup.geese.io.linkup.cache.IDataLoadedCallable;
 import linkup.geese.io.linkup.data.Link;
+import linkup.geese.io.linkup.data.Location;
+import linkup.geese.io.linkup.data.Proficiency;
 import linkup.geese.io.linkup.data.User;
+import linkup.geese.io.linkup.data.UserType;
 
 public class MainActivity extends AppCompatActivity implements IDataLoadedCallable {
 
@@ -19,33 +22,51 @@ public class MainActivity extends AppCompatActivity implements IDataLoadedCallab
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
-        Map<String, Link> arr = new HashMap<>();
-        User newUser = new User(1, "Archer", "Zhang", "some@email.com", arr);
         Cache cache = Cache.getInstance(this);
-        cache.setUser(1, newUser);
+
+        Map<String, Link> link1 = new HashMap<>();
+
+        link1.put("_0", new Link(1, 3.23232323243, 24.3225432, Calendar.getInstance().getTimeInMillis()));
+
+        Map<String, Link> link2 = new HashMap<>();
+
+        link2.put("_1", new Link(2, 3.23232323243, 24.3225432, Calendar.getInstance().getTimeInMillis()));
+
+//
+//        Map<String, Proficiency> mSkills,
+//        Map<String, String> mContacts,
+//        Map<String, Link> mLinks,
+//        Location mLocation) {
+
+        Map<String, Proficiency> skills1 = new HashMap<>();
+        skills1.put("C++", Proficiency.HIGH);
+        skills1.put("Web", Proficiency.LOW);
+
+        Map<String, String> contact1 = new HashMap<>();
+        contact1.put("Phone", "2232232233");
+        contact1.put("Email", "archer@email.com");
+
+        Location location1 = new Location(32.232325D, 64.235, 1, Calendar.getInstance().getTimeInMillis());
+
+        cache.setUser(1, new User(0, "Archer", "Zhang", UserType.CANDIDATE, skills1, contact1, link1, location1));
+
+
+
+        Map<String, Proficiency> skills2 = new HashMap<>();
+        skills1.put("Android", Proficiency.HIGH);
+        skills1.put("Web", Proficiency.LOW);
+
+        Map<String, String> contact2 = new HashMap<>();
+        contact1.put("Phone", "2662232233");
+        contact1.put("Email", "lpan@email.com");
+
+        Location location2 = new Location(32.232325D, 64.235, 1, Calendar.getInstance().getTimeInMillis());
+
+
+        cache.setUser(2, new User(2, "Lawrence", "Pan", UserType.RECRUITER, skills2, contact2, link2, location2));
+
         cache.commit();
-        cache.getUser(1);
-        cache.setUser(1, new User(1, "Archer", "Zhang", "someother@email.com", arr));
-        cache.commit();
-        cache.getUser(1);
-        cache.setUser(2, new User(2, "Lawrence", "Pan", "lpan@email.com", arr));
-        cache.commit();
 
-        Map<String, Link> arr1 = new HashMap<>();
-
-        arr1.put("_0", new Link(1, 3.23232323243, 24.3225432, Calendar.getInstance().getTimeInMillis()));
-
-        Map<String, Link> arr2 = new HashMap<>();
-
-        arr2.put("_1", new Link(2, 3.23232323243, 24.3225432, Calendar.getInstance().getTimeInMillis()));
-
-
-        cache.setUser(1, new User(1, "Archer", "Zhang", "someother@email.com", arr1));
-        cache.setUser(2, new User(2, "Lawrence", "Pan", "lpan@email.com", arr2));
-
-        cache.commit();
-
-        cache.getUser(2);
 
     }
 
